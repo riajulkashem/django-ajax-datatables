3c55129aafad450f601a79b176d1944c83bb0b3e
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-28 23:34:52 +0600

    updated readme

---
+++ sapience_trio/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sapience_trio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sapience_trio.settings')
+
+application = get_wsgi_application()
