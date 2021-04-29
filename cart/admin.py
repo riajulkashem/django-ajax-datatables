593759e2436217db02156355fcc321c0f6adffba
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-29 20:28:00 +0600

    model registerd in admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from cart.models import Item, OrderItem, Order
+
+admin.site.register(Item)
+admin.site.register(OrderItem)
+admin.site.register(Order)
