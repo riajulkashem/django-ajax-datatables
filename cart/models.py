e0e829f23431abdda97fcd3eaa97f378f7599ce3
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-29 20:15:25 +0600

    TimeStampedModel Created

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    creator = models.ForeignKey(User, on_delete=models.PROTECT)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['updated']
