ea00921adb163431499ab6675aa4f2f1549c6bd9
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-30 01:06:31 +0600

    datatable search and filter method implemented

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import ListView
 from rest_framework.generics import ListAPIView
@@ -18,3 +19,28 @@ class OrderListAPIView(ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    def datatable_search(self, queryset):
+        """
+        :param queryset:
+        :return: queryset by filtering search value
+        """
+        search_value = self.request.query_params.get('search')
+        if search_value:
+            if search_value.isdigit():
+                return queryset.filter(
+                    Q(order_num=int(search_value)) |
+                    Q(total_item=int(search_value)) |
+                    Q(total_price=int(search_value))
+                )
+            return queryset.filter(client_name__icontains=search_value)
+        return queryset
+
+    def datatable_filter(self, queryset):
+        """
+        :param queryset:
+        :return: queryset order by order_value
+        """
+        order_value = self.request.query_params.get('order')
+        if order_value:
+            return queryset.order_by(order_value)
+        return queryset
