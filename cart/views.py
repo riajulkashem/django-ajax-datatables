1609c73c9cdcbdf8bc68294d95667b38f8f66215
Riajul Kashem <riajul.programmer@gmail.com>
2021-04-30 01:26:17 +0600

    order list view using jQuery server side implementation done :)

--- cart/views.py
+++ cart/views.py
@@ -19,6 +19,8 @@ class OrderListAPIView(ListAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+    # ordering_fields = ('order_num', 'client_name', 'total_item', 'total_price')
+
     def datatable_search(self, queryset):
         """
         :param queryset:
@@ -27,11 +29,8 @@ class OrderListAPIView(ListAPIView):
         search_value = self.request.query_params.get('search')
         if search_value:
             if search_value.isdigit():
-                return queryset.filter(
-                    Q(order_num=int(search_value)) |
-                    Q(total_item=int(search_value)) |
-                    Q(total_price=int(search_value))
-                )
+                # Model Object Cant Filter or Order By Model Property
+                return [q for q in queryset if int(search_value) in [q.order_num, q.total_item, q.total_price]]
             return queryset.filter(client_name__icontains=search_value)
         return queryset
 
@@ -42,5 +41,11 @@ class OrderListAPIView(ListAPIView):
         """
         order_value = self.request.query_params.get('order')
         if order_value:
-            return queryset.order_by(order_value)
+            # Model Object Cant Filter or Order By Model Property
+            return sorted(queryset, key=lambda row: getattr(row, order_value))
         return queryset
+
+    def get_queryset(self):
+        queryset = super(OrderListAPIView, self).get_queryset()
+        searched_queryset = self.datatable_search(queryset)
+        return self.datatable_filter(searched_queryset)
